9578028172d3b2481b9983344e6a21b3f16c52d7
ProgAli <51164107+progali007@users.noreply.github.com>
2022-04-14 13:42:20 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-    <?php
-        header('Content-Type: application/json');
-        header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-        header("Cache-Control: post-check=0, pre-check=0", false);
-        header("Pragma: no-cache");
-    ?>
+   
     let day_count = 13;
     for (i = 1; i <= day_count; i++) {
         $('.list-group').append(`<li class="list-group-item"><a href="day${('000' + i).slice(-3)}">Day ${i}</a></li>`);
