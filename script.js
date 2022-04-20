bfe6dba197308d52202aabfd094ad9a48397d5c7
Moein Aghamirzaei <moein.6556@gmail.com>
2022-04-20 11:39:04 +0430

    add dynamic pages with github api

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 $(document).ready(function () {
-   
-    let day_count = 15;
-    for (i = 1; i <= day_count; i++) {
-        $('.list-group').append(`<li class="list-group-item"><a href="day${('000' + i).slice(-3)}">Day ${i}</a></li>`);
-    }
+    var api_url = 'https://api.github.com/repos/progali007/100-DAYS-CSS-CHALLENGE/git/trees/master'
+    const regex = new RegExp('^day[0-9]{3}$');
+    count_day = 1
+    $.get(api_url, function (data, status) {
+        items = data['tree']
+        for (var i of items) {
+            var name = i['path']
+            if (regex.test(name)) {
+                $('.list-group').append(`<li class="list-group-item"><a href="${name}">Day ${count_day}</a></li>`);
+                count_day += 1
+            }
+        }
+    });
 });
