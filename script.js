17c773cc7165b3f6b57b3e1e713ecdcb16994f98
progali007 <code.progali@gmail.com>
2022-05-04 16:31:39 +0430

    Change syntax to ES6

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
     var api_url = 'https://api.github.com/repos/progali007/100-DAYS-CSS-CHALLENGE/git/trees/master'
     const regex = new RegExp('^day[0-9]{3}$');
     count_day = 1
-    $.get(api_url, function (data, status) {
+    $.get(api_url, (data, status) => {
         items = data['tree']
-        for (var i of items) {
-            var name = i['path']
+        items.forEach(elm => {
+            let name = elm['path']
             if (regex.test(name)) {
                 $('.list-group').append(`<li class="list-group-item"><a href="${name}">Day ${count_day}</a></li>`);
                 count_day += 1
             }
-        }
+        });
     });
 });
